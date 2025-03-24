8d3697081567f4eaced70f6362f3a2f0d2c65e6c
Giacomo Marciani <mgiacomo@amazon.com>
2025-03-24 09:50:45 -0400

    debug changes

--- src/slurm_plugin/computemgtd.py
+++ src/slurm_plugin/computemgtd.py
@@ -223,6 +223,8 @@ def _run_computemgtd(config_file):
                 _self_terminate()
         else:
             log.info("XXXXXXXXXX MODIFIED BY MGIACOMO -- self terminate skipped because expired_clustermgtd_heartbeat is False XXXXXXXXXX")
+        log.info("XXXXXXXXXX MGIACOMO - reenable interface XXXXXXXXXX")
+        run_command("sudo ifconfig ens5 up")
         sleep_remaining_loop_time(computemgtd_config.loop_time, current_time)
 
 
